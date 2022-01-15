0a9a9e16d87696e03d0a591f49e3c2bed33f7767
PawelPaziewski <lrpapawel@gmail.com>
2022-01-15 18:42:16 +0100

    Implement database

--- app/src/main/java/pl/paziewski/mobileapp/Results.kt
+++ app/src/main/java/pl/paziewski/mobileapp/Results.kt
@@ -2,10 +2,19 @@ package pl.paziewski.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class Results : AppCompatActivity() {
+
+    private lateinit var database: AppDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        database = Room.databaseBuilder(applicationContext, AppDb::class.java, "result")
+            .allowMainThreadQueries().build()
+
+        val results = database.resultDao().getAllOrderedByScore()
     }
 }
